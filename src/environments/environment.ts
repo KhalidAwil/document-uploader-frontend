7363d9ed52e8a6fe4dacdbb22d76658afb37fc30
KhalidAwil <khalidawil3@gmail.com>
2025-12-23 15:18:50 -0500

    Force git reset in deployment and update dev env to match staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api',
-    fileUploadUrl: 'http://localhost:8000/storage',
-    plausibleUrl: 'http://localhost:8080'
+    apiUrl: 'https://api-staging-uploader.ynaqyal.info/api',
+    fileUploadUrl: 'https://api-staging-uploader.ynaqyal.info/storage',
+    plausibleUrl: ''
 };
