dbce8063d3a1bb26a680be3484260518dccfe1ac
KhalidAwil <khalidawil3@gmail.com>
2025-12-23 14:50:59 -0500

    Add missing fileUploadUrl and plausibleUrl to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api-staging-uploader.ynaqyal.info/api'
+    apiUrl: 'https://api-staging-uploader.ynaqyal.info/api',
+    fileUploadUrl: 'https://api-staging-uploader.ynaqyal.info/storage',
+    plausibleUrl: '' // Not configured for staging
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api'
+    apiUrl: 'http://localhost:8000/api',
+    fileUploadUrl: 'http://localhost:8000/storage',
+    plausibleUrl: 'http://localhost:8080'
 };
