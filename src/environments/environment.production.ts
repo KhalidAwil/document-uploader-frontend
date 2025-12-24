ed3bc9bba45ce7c682d7412a1f134f7b4875b304
KhalidAwil <khalidawil3@gmail.com>
2025-12-24 15:17:15 -0500

    fix(env): update api url to use v1 prefix

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api-uploader.ynaqyal.info/api',
+    apiUrl: 'https://api-uploader.ynaqyal.info/api/v1',
     fileUploadUrl: 'https://api-uploader.ynaqyal.info/storage',
     plausibleUrl: '' // Configure if using Plausible analytics in production
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://api-staging-uploader.ynaqyal.info/api',
+    apiUrl: 'https://api-staging-uploader.ynaqyal.info/api/v1',
     fileUploadUrl: 'https://api-staging-uploader.ynaqyal.info/storage',
     plausibleUrl: ''
 };
