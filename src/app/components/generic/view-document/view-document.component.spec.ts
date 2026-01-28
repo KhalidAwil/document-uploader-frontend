842146039f410734f28f54c1e94cbead1b656929
KhalidAwil <khalidawil3@gmail.com>
2026-01-28 13:48:02 -0500

    fix(view-document): restore image modal using NgbModal and fix click issues

--- src/app/components/generic/view-document/view-document.component.spec.ts
+++ src/app/components/generic/view-document/view-document.component.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('ViewDocumentComponent', () => {
   let component: ViewDocumentComponent;
@@ -25,6 +26,13 @@ describe('ViewDocumentComponent', () => {
               data: { modelType: 'news' }
             },
             params: of({ id: '1' })
+          }
+        },
+        {
+          provide: NgbModal,
+          useValue: {
+            open: () => ({ result: Promise.resolve() }),
+            dismissAll: () => { }
           }
         }
       ]
